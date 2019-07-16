7e2574e2737dabba83408e77cd265dd91d2102e5
Yiraneva <xueyiran123@gmail.com>
2019-07-17 00:48:03 +0800

    Udacity exercise

---
+++ Udacity-Python-Set.py
@@ -0,0 +1,7 @@
+a = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+b = set(a)
+print(b)
+b.add(5)
+
+b.pop()
+print(b)
---
+++ Udacity-PythonList.py
@@ -0,0 +1,5 @@
+new_str = "\n".join(["fore", "aft", "starboard", "port"])
+print(new_str)
+
+new_str = "klklhjh".join(["fore", "aft", "starboard", "port"])
+print(new_str)
