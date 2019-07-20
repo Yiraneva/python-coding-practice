68007905bd36780816a8bba9f3d405e3cfc35c05
Yiraneva <xueyiran123@gmail.com>
2019-07-20 23:52:21 +0800

    Update

--- Udacity_Py_Boolean4controlflow.py
+++ Udacity_Py_Boolean4controlflow.py
@@ -1,3 +1,9 @@
+# Truth: String
+# False: None
+# 
+#  https://classroom.udacity.com/courses/ud1110/lessons/8655bee4-19e1-4de0-8177-4f895a74b57b/concepts/69bfbde2-3f63-41ed-8793-aabf0f6a4bab
+
+
 points = 174
 
 if points <= 50:
@@ -9,7 +15,7 @@ elif points <= 180:
 else:
     prize = "penguin"
 
-if points:
+if prize:
     result = "Congratulations! You won a {}!".format(prize)
 else:
     result = "Oh dear, no prize this time."
