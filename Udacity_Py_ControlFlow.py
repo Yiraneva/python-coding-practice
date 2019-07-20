037e24045474c73ac8f40c9816a80089ce7cbea7
Yiraneva <xueyiran123@gmail.com>
2019-07-20 23:38:09 +0800

    update

--- Udacity_Py_ControlFlow.py
+++ Udacity_Py_ControlFlow.py
@@ -1,3 +1,6 @@
+#注意每一个 if 和 elif的顺序。只有上一个false，才会往下走。
+#https://classroom.udacity.com/courses/ud1110/lessons/8655bee4-19e1-4de0-8177-4f895a74b57b/concepts/5a45f230-6087-4d0e-9e1a-3ddd4fc664e3
+
 points = 174  # use this input to make your submission
 
 # write your if statement here
---
+++ Udacity_Py_ifStatement.py
@@ -0,0 +1,32 @@
+#Conditional Statement
+#Be ware that "=" and "==" are different!!!
+
+#https://classroom.udacity.com/courses/ud1110/lessons/8655bee4-19e1-4de0-8177-4f895a74b57b/concepts/44a22f87-f7ce-4a9a-a1f5-86024edb0f29
+
+# '''
+# Depending on where an individual is from we need to tax them 
+# appropriately.  The states of CA, MN, and 
+# NY have taxes of 7.5%, 9.5%, and 8.9% respectively.
+# Use this information to take the amount of a purchase and 
+# the corresponding state to assure that they are taxed by the right
+# amount.
+# '''
+state = 'CA'
+purchase_amount = 50
+
+if state == 'CA':
+    tax_amount = .075
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+elif state == 'MN':
+    tax_amount = .095
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+elif state== 'NY':
+    tax_amount = .089
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+print(result)
