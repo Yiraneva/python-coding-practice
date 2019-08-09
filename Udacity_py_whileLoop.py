71c43541d1ed662f0af3ae1bbfa29c8357cb184d
Xue Yiran <xueyiran@Xues-MacBook.local>
2019-08-10 00:09:25 +0800

    Udacity Practice

---
+++ Udacity_py_whileLoop.py
@@ -0,0 +1,14 @@
+#https://classroom.udacity.com/courses/ud1110/lessons/8655bee4-19e1-4de0-8177-4f895a74b57b/concepts/f9030dc6-d2f3-4076-bb80-a0ed9697ecad
+
+# number to find the factorial of
+number = 6   
+
+# start with our product equal to one
+product = 1
+while number > 0:
+    product = product * number
+    number = number - 1
+ 
+    
+
+print(product)
